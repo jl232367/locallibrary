9f20dbf5d53a489bf9565de326027d3e63427098
jsl.luallen@gmail.com <jsl.luallen@gmail.com>
2021-11-10 09:39:26 -0500

    Added connection to the library MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 var createError = require('http-errors');
 var express = require('express');
+//Set up mongoose connection
+var mongoose = require('mongoose');
+var mongoDB = 'mongodb+srv://m001-student:m001-mongodb-basics@librarycluster.c6e2g.mongodb.net/local_library?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
